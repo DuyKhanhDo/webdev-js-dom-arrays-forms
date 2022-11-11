893724d6332b2809c39caac5f5f4c31abdd68ae1
DuyKhanhDo <116875186+DuyKhanhDo@users.noreply.github.com>
2022-11-11 10:19:22 +0000

    huge update for js

--- js/main.js
+++ js/main.js
@@ -3,5 +3,12 @@
 //start
 document.getElementById("myHeading").innerHTML="Keith"
 firstlistItem=document.querySelector("nav ul li").setAttribute("Class","currentPage")
+console.info(firstlistItem)
+firstlistItem.setAttribute("class","currentPage")
+let firstLink=document.querySelector("nav ul li a")
+firstLink.setAttribute("href","https://www.google.co.uk")
+//add test event
+document.getElementById("myTestEvent").addEventListener("click", myfunction)
+console.info("hi")
 //end
 })()
